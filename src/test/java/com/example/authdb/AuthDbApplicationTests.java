c931a17825c3c493641d4e20b38e1eb6512463e5
sabyasachi mohanty <mailtosabya.29@gmail.com>
2021-04-25 23:08:14 +0530

    Initial commit: code

---
+++ src/test/java/com/example/authdb/AuthDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
